f4f359358da82ca4108050a30c7dbfeaabf63000
Madmaxvas <maxmadvas@gmail.com>
2026-03-26 02:28:21 +0300

    Fifth commit

--- vasilenko.maxim/S2/Queue.hpp
+++ vasilenko.maxim/S2/Queue.hpp
@@ -1,13 +1,13 @@
-#ifndef COMMON_STACK_HPP
-#define COMMON_STACK_HPP
+#ifndef COMMON_QUEUE_HPP
+#define COMMON_QUEUE_HPP
 #include "List.hpp"
 namespace vasilenko {
   template <typename T>
-  class Stack {
+  class Queue {
   public:
-    void push(const T& rhs) { list_.pushFront(rhs); }
+    void push(const T& rhs) { list_.pushBack(rhs); }
     void pop() { list_.popFront(); }
-    const T& getTop() const { return list_.getFront(); }
+    const T& getFront() const { return list_.getFront(); }
     bool isEmpty() const { return list_.isEmpty(); }
   private:
     List<T> list_;
