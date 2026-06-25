fd1132e3c0dc6d1dd187b20c3c55abc75fd508ec
Madmaxvas <maxmadvas@gmail.com>
2026-06-25 05:47:01 +0300

    Sixth commit iterators Rtree

---
+++ vasilenko.maxim/F0/RTree_Iterators.hpp
@@ -0,0 +1,74 @@
+#ifndef RTREE_ITERATORS_HPP
+#define RTREE_ITERATORS_HPP
+
+#include "Location.hpp"
+#include "Vector.hpp"
+#include <cstddef>
+#include <iterator>
+#include <memory>
+
+namespace vasilenko {
+  namespace detail {
+
+    struct RTreeNode;
+
+    class RTreeIterator {
+    public:
+      using iterator_category = std::forward_iterator_tag;
+      using value_type = Location;
+      using difference_type = std::ptrdiff_t;
+      using pointer = Location*;
+      using reference = Location&;
+
+    private:
+      Vector<std::shared_ptr<RTreeNode>> nodeStack_;
+      std::size_t locIndex_;
+      Location* currentLoc_;
+
+      void advance();
+
+    public:
+      RTreeIterator(std::shared_ptr<RTreeNode> root, bool isEnd);
+
+      reference operator*() { return *currentLoc_; }
+      pointer operator->() { return currentLoc_; }
+
+      RTreeIterator& operator++();
+      RTreeIterator operator++(int);
+
+      bool operator==(const RTreeIterator& other) const { return currentLoc_ == other.currentLoc_; }
+      bool operator!=(const RTreeIterator& other) const { return currentLoc_ != other.currentLoc_; }
+    };
+
+    class RTreeConstIterator {
+    public:
+      using iterator_category = std::forward_iterator_tag;
+      using value_type = const Location;
+      using difference_type = std::ptrdiff_t;
+      using pointer = const Location*;
+      using reference = const Location&;
+
+    private:
+      Vector<std::shared_ptr<RTreeNode>> nodeStack_;
+      std::size_t locIndex_;
+      const Location* currentLoc_;
+
+      void advance();
+
+    public:
+      RTreeConstIterator(std::shared_ptr<RTreeNode> root, bool isEnd);
+
+      reference operator*() const { return *currentLoc_; }
+      pointer operator->() const { return currentLoc_; }
+
+      RTreeConstIterator& operator++();
+      RTreeConstIterator operator++(int);
+
+      bool operator==(const RTreeConstIterator& other) const { return currentLoc_ == other.currentLoc_; }
+      bool operator!=(const RTreeConstIterator& other) const { return currentLoc_ != other.currentLoc_; }
+    };
+
+  }
+}
+
+#endif
